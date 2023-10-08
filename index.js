250fa19ceb4405ba6cf22291fe1513db289a4f78
Matthew Houston <matthouston027@gmail.com>
2023-10-07 23:10:38 -0500

    Add routing to backend

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const express = require("express");
+const app = express();
 
+//Express provides request and response objects in the callback
+// app.use((req, res) => {
+//     console.log("We got a new request!");
+//     res.send("Hello, we got your request!")
+// })
+
+
+//Only matching get requests
+//Path routing to specific animal returns a specific request
+app.get('/', (req, res) => {
+    res.send('This is the homepage!!')
+})
+
+app.get('/cats', (req, res) => {
+    res.send('MEOW!!')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF!!')
+})
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+})
