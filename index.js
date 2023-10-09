5c8d9138865b9365acf69242c3d98d39408a380c
Matthew Houston <matthouston027@gmail.com>
2023-10-09 16:22:12 -0500

    Add subreddit pages fro json and error handling on missing data

--- index.js
+++ index.js
@@ -56,6 +56,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views')) //Sets the default directory name for /views folder
@@ -63,6 +64,28 @@ app.set('views', path.join(__dirname, '/views')) //Sets the default directory na
 app.get('/', (req, res) => {
     // res.send("HI")
     res.render('home') //'home.ejs' or 'home' will both work
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { rand: num }) //'home.ejs' or 'home' will both work
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit', { ...data })
+    } else {
+        res.render('notfound', { subreddit });
+    }
+})
+
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
+    ]
+    res.render('cats', { cats })
 })
 
 app.listen(3000, () => {
